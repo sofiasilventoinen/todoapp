c8c023f7839e304ef10e1b1ff1f696dd9d1ebbd6
sofiasilventoinen <sofiasilventoinen@hotmail.com>
2023-01-03 12:00:05 +0100

    Todo

---
+++ src/Components/ToDoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Todo from "./Todo";
+
+const ToDoList = ({ todos, setTodos, filteredTodos }) => {
+    return(
+        <div className="todo-container">
+        <ul className="todo-list">
+            {filteredTodos.map(todo => (
+                <Todo 
+                key={todo.id} 
+                text={todo.text} 
+                setTodos={setTodos} 
+                todos={todos}
+                todo={todo}
+                />
+            ))}
+        </ul>
+        
+        </div>
+    );
+};
+
+export default ToDoList;
